0ee1a1f24301d66250189431ffd1fd69fd98b52d
Anderson Tardelli <168490750+AnderRz@users.noreply.github.com>
2024-05-13 21:17:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 alert("Seja bem vindo");
 function contato(){
-  alert("Seja bem vindo(a), "+document.queryselector("#nome").value+"!");
+  alert("Seja bem vindo(a), "+document.querySelector("#nome").value+"!");
 }
